0dc339b06bd27e17c7c8b2751d2da6ab0efafc1b
Tanat Boozayaangool <ttanatb@gmail.com>
2022-01-02 14:57:51 -0800

    Add LayerMask & Mathr

---
+++ Scripts/LayerMask.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utilr
+{
+    public static class LayerMaskExtensions
+    {
+
+        public static bool ContainsLayer(this LayerMask mask, int layer)
+        {
+            return mask == (mask | (1 << layer));
+        }
+    }
+}
---
+++ Scripts/Mathr.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mathr
+{
+    public static float Damp(float a, float b, float lambda, float dt)
+    {
+        return Mathf.Lerp(a, b, 1 - Mathf.Exp(-lambda * dt));
+    }
+}
