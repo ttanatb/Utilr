eecf2b52264d7470ee41f886021f10ccbb9cdc62
Tanat Boozayaangool <ttanatb@gmail.com>
2023-08-18 17:03:18 +0100

    Add auto invoke on start for SoGameEvent (for testing)

--- Scripts/SoGameEvents/SoGameEventBase.cs
+++ Scripts/SoGameEvents/SoGameEventBase.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Threading.Tasks;
 using NaughtyAttributes;
 using UnityEngine;
 
@@ -10,5 +12,26 @@ namespace Utilr.SoGameEvents
         /// </summary>
         [Button("Test Invoke")]
         public abstract void Invoke();
+
+        /// <summary>
+        /// Event will auto-invoke on Start, definitely more useful for testing.
+        /// </summary>
+        [SerializeField]
+        private bool m_invokeOnStart = false;
+
+        protected virtual void Awake()
+        {
+            DelayedInvoke();
+        }
+        
+        /// <summary>
+        /// Hack to invoke event after 1 second.
+        /// </summary>
+        /// <param name="seconds"></param>
+        private async void DelayedInvoke(int seconds = 1)
+        {
+            await Task.Delay(seconds * 1000);
+            Invoke();
+        }
     }
 }
